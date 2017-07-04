749472ab10900ef8fae2adff2407b6b5d6580c4f
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-07-04 20:00:06 +0700

    build react-native-initial

---
+++ ReactNativeApp/index.android.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import {
+  AppRegistry,
+  StyleSheet,
+} from 'react-native';
+
+export default class ReactNativeApp extends Component {
+  constructor(){
+    super()
+  }
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Text>
+            This is Content Section
+          </Text>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ReactNativeApp', () => ReactNativeApp);
